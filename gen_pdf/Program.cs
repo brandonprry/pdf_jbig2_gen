66eb3f61607806c005d87ae7c5aa366d0081da95
Brandon Perry <bperry.volatile@gmail.com>
2016-11-06 16:35:06 -0600

    pdfgen

---
+++ gen_pdf/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace gen_pdf
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string file = args[0];
+			byte[] data = File.ReadAllBytes(file);
+
+			var assembly = Assembly.GetExecutingAssembly();
+			var resourceName = "gen_pdf.300dpiBW-1-Halftone.txt";
+
+			string template = string.Empty;
+			using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+			using (StreamReader reader = new StreamReader(stream))
+				template = reader.ReadToEnd();
+
+			string hex = string.Concat(data.Select(b => b.ToString("X2")).ToArray()).ToLower();
+			template = template.Replace("{JBIG_LENGTH}", hex.Length.ToString());
+
+			template = template.Replace("{JBIG_IMAGE}", hex);
+
+			int idx = template.IndexOf("10 0 obj");
+			string sidx = idx.ToString();
+			sidx = sidx.PadLeft(10, '0');
+			template = template.Replace("{10_OBJ_OFFSET}", sidx);
+
+			idx = template.IndexOf("xref");
+			int idx2 = Regex.Match(template, "xref").Index;
+
+			template = template.Replace("{XREF_OFFSET}", idx.ToString());
+
+			string name = Guid.NewGuid().ToString();
+			File.WriteAllText("/tmp/" + name + ".pdf", template);
+
+			Console.WriteLine("File written to /tmp/" + name + ".pdf");
+		}
+	}
+}
