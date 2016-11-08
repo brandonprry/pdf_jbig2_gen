baa086f4a9f98c271f1734720a289c1f9006f0a4
Brandon Perry <bperry.volatile@gmail.com>
2016-11-08 17:17:07 -0600

    * 300dpiBW-1-Halftone.txt: updates
    * Program.cs:

--- gen_pdf/Program.cs
+++ gen_pdf/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -20,7 +21,18 @@ namespace gen_pdf
 			using (StreamReader reader = new StreamReader(stream))
 				template = reader.ReadToEnd();
 
-			string hex = string.Concat(data.Skip(8).Select(b => b.ToString("X2")).ToArray()).ToLower() + ">";
+			data = data.Skip(8).ToArray(); //header info
+
+			byte[] seg0 = data.Take(16).ToArray();
+
+			string globalhex = string.Concat(
+				seg0.Select(b => b.ToString("X2")).ToArray()).ToLower() + ">";
+			template = template.Replace("{GLOBAL_HEX}", globalhex);
+			template = template.Replace("{GLOBAL_LENGTH}", globalhex.Length.ToString());
+
+			data = data.Skip(16).ToArray();
+
+			string hex = string.Concat(data.Select(b => b.ToString("X2")).ToArray()).ToLower() + ">";
 			template = template.Replace("{JBIG_LENGTH}", hex.Length.ToString());
 			template = template.Replace("{JBIG_IMAGE}", hex);
 
@@ -29,6 +41,11 @@ namespace gen_pdf
 			sidx = sidx.PadLeft(10, '0');
 			template = template.Replace("{10_OBJ_OFFSET}", sidx);
 
+			idx = template.IndexOf("11 0 obj");
+			sidx = idx.ToString();
+			sidx = sidx.PadLeft(10, '0');
+			template = template.Replace("{11_OBJ_OFFSET}", sidx);
+
 			idx = template.IndexOf("xref");
 			template = template.Replace("{XREF_OFFSET}", idx.ToString());
 
@@ -36,5 +53,6 @@ namespace gen_pdf
 			File.WriteAllText("/tmp/" + name + ".pdf", template);
 			Console.WriteLine("File written to /tmp/" + name + ".pdf");
 		}
+
 	}
 }
